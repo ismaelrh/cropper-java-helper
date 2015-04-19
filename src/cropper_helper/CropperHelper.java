be6a49f275e36224e503733be4e9752fbc6e24c0
dbarelop <dbarelop@gmail.com>
2015-04-19 19:03:59 +0200

    NASA cached thermical anomalies data updated on demand

--- src/cropper_helper/CropperHelper.java
+++ src/cropper_helper/CropperHelper.java
@@ -1,17 +1,8 @@
 package cropper_helper;
 
-import cropper_helper.crawling.threads.ThermalAnomaliesTask;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 public class CropperHelper {
-    private static final long THANOM_STATS_FREQ = 24*60*60*1000;    // 1 day
 
     public static void main(String[] args) {
         new Thread(new ListenServer()).start();
-        ScheduledExecutorService scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutor.scheduleAtFixedRate(new ThermalAnomaliesTask(), 0, THANOM_STATS_FREQ, TimeUnit.MILLISECONDS);
     }
 }
--- src/cropper_helper/crawling/NASADataCrawler.java
+++ src/cropper_helper/crawling/NASADataCrawler.java
@@ -21,30 +21,29 @@ import java.util.logging.Logger;
  */
 public class NASADataCrawler {
     private static final Logger logger = Logger.getLogger(NASADataCrawler.class.getName());
-    private static final long THANOM_STATS_FREQ = 24*60*60*1000;    // 1 day
     private static final String DATA_URL = "https://api.data.gov/nasa/planetary/earth/temperature/coords?lon=%f&lat=%f&begin=1990&end=%d&api_key=DEMO_KEY";
 
     public static Map<String,Object> updateThermalAnomaly(Coordinate mid_point, String _id) {
         try {
             JsonObject old_data = DatabaseHelper.getThermalDocument(_id);
-            if (old_data == null || (System.currentTimeMillis() - old_data.get("timestamp").getAsLong() > THANOM_STATS_FREQ)) {
-                /*String urlString = "https://api.data.gov/nasa/planetary/earth/temperature/coords?lon=" +
-                    mid_point.x%180 + "&lat=" +
-                    mid_point.y%180 + "&begin=1990&end="+ Calendar.getInstance().get(Calendar.YEAR)+"&api_key=DEMO_KEY";*/
-                URL url = new URL(String.format(Locale.US, DATA_URL, mid_point.x % 180, mid_point.y % 180, Calendar.getInstance().get(Calendar.YEAR)));
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            if (old_data == null || (currentYear > old_data.get("year").getAsInt())) {
+                // Update cache version
+                URL url = new URL(String.format(Locale.US, DATA_URL, mid_point.x % 180, mid_point.y % 180, currentYear));
                 try (InputStream is = url.openConnection().getInputStream()) {
                     if (old_data != null) {
                         DatabaseHelper.removeDoc(old_data);
                     }
                     Map<String, Object> jsonMap = new ObjectMapper().readValue(is, Map.class);
                     jsonMap.put("_id", _id);
-                    jsonMap.put("timestamp", System.currentTimeMillis());
+                    jsonMap.put("year", currentYear);
 
                     DatabaseHelper.storeDoc(jsonMap);
                     logger.log(Level.FINE, "Thermal anomaly information stored in the database");
                     return jsonMap;
                 }
             } else if (old_data != null) {
+                // Obtain data from cached version
                 return new ObjectMapper().readValue(old_data.toString(), HashMap.class);
             } else {
                 return null;
@@ -54,7 +53,6 @@ public class NASADataCrawler {
             return null;
         }
     }
-
 
     private static ArrayList<ThermalSingleValue> readArray(JsonReader reader) throws IOException {
         ArrayList<ThermalSingleValue> result = new ArrayList<>();
